ba718dfac7ed89593555cdf40d7d5961016dc545
antoniocuga <antoniocuga@gmail.com>
2014-08-16 13:27:15 -0500

    Like

--- unidad3/class03/js/script.js
+++ unidad3/class03/js/script.js
@@ -16,28 +16,52 @@ $().ready(function() {
 
         posts[1] = message;
 
+        //Creando elementos de comentario. Like, Like count and delete.
         var like = document.createElement("a");
+        var likecount = document.createElement("span");
         var del = document.createElement("a");
 
-        $(like).attr({'class': 'like', 'href': '#', 'comment-id': id});
-        $(like).html("Like");
 
+        //Añadiendo propiedades al boton like
+        $(like).attr({'class': 'like', 'href': '#', 'comment-id': id});
+        $(like).html("Like: ");
+
+        //Añadiendo propiedades al boton eliminar
         $(del).attr({'class': 'eliminar', 'href': '#', 'comment-id': id});
         $(del).html('Eliminar');
 
+
+        //Añadiendo cantidad de likes
+        $(likecount).html('0');
+
+        //insertando likecount a el boton de like.
+        $(like).append(likecount);
+
+        //añadiendo links a nuestro comentario.
         $(itemComment).append([del, like])
 
+        //Insertando los comentarios en la lista. (Orden inverso)
         $(commentList).prepend(itemComment);
 
+        //limpiando nuestro textarea.
         $('#comment').val('');
 
+        //Incrementando el ID
         id++;
 
     });
 
 
     $('#post-list').on('click', '.item-comment > a.like', function(event) {
-        alert("aca");
+
+        var id = $(this).attr('comment-id');
+
+        var comment = $('#comment-' + id).find('.like > span');
+
+        var count = comment.html();
+        count = parseInt(count);
+        comment.html((count + 1));
+
     });
 
 
