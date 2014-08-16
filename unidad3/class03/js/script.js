e451975b3989ffd00b200c47fd3bcb7610c16f07
antoniocuga <antoniocuga@gmail.com>
2014-08-16 12:33:32 -0500

    Añadiendo ejemplo de comentarios

--- unidad3/class03/js/script.js
+++ unidad3/class03/js/script.js
@@ -1,7 +1,35 @@
-$().ready(function(){
-    
-        
-    
-    
-    
+$().ready(function() {
+
+    $('#send-post').click(function() {
+
+        var message = $('#comment').val();
+
+        var commentList = $('#post-list');
+
+        var itemComment = document.createElement("div");
+
+        $(itemComment).attr('class', 'item-comment');
+
+        $(itemComment).html(message);
+
+
+        var like = document.createElement("a");
+        var del = document.createElement("a");
+
+        $(like).attr({'class': 'like', 'href': '#'});
+        $(like).html("Like");
+
+        $(del).attr({'class': 'eliminar', 'href': '#'});
+        $(del).html('Eliminar');
+
+        $(itemComment).append([del, like])
+
+        $(commentList).prepend(itemComment);
+
+        $('#comment').val('');
+
+    });
+
+
+
 });
